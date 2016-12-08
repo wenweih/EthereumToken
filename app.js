66abbc23cb190bc47165bd89128e20e364d2437a
hww <836967373@qq.com>
2016-12-08 11:47:28 +0800

    append tokenAbi, contract_address to abi.js file

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ var token_source_code = fs.readFileSync("token.sol", "utf8");
 var tokenCompile = web3.eth.compile.solidity(token_source_code);
 var token_contract = web3.eth.contract(tokenCompile.Token.info.abiDefinition);
 
+var abi_file = 'abi.js';
+
 //web3.personal.unlockAccount(coinbase, "W&qQ6Nb3Wj{X}", 1000);
 
 var initializer = {
@@ -21,12 +23,15 @@ var initializer = {
 var deploy_callback = function(e, contract){
   if(!e){
     if(!contract.address){
-      console.log("Contract transactionhash:" + contract.transactionHash + "wait to be minded");
+      console.log("Contract transactionhash:" + contract.transactionHash + " wait to be minded");
     }else {
       console.log("contract minded, address:" + contract.address);
       contract.issue.sendTransaction(coinbase, 500, {from: coinbase}, function(){
-        console.log("issue 500")
-      })
+        console.log("issue 500");
+      });
+
+      fs.appendFileSync(abi_file, "var tokenAbi = " + JSON.stringify(contract.abi) + ";\r\n");
+      fs.appendFileSync(abi_file, "var contract_address = \"" + contract.address + "\";\r\n");
     }
   }
 };
